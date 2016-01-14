11f22a1bb2b7f3db5115b36586c04691e75325b3
Robert-Jan Keizer <robertjankeizer@gmail.com>
2016-01-14 10:33:53 +0100

    Init commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var chalk = require('chalk');
+var shell = require('shelljs');
+var readline = require('readline');
+
+var rl = readline.createInterface(process.stdin, process.stdout);
+
+rl.setPrompt(chalk.green('Android') + ' ' + chalk.red('Keyboard') + ' by ' + chalk.blue('KeizerDev') + '\nText> ');
+rl.prompt();
+
+rl.on('line', function(line) {
+    shell.exec("adb shell \"input keyboard text '" + line + "'\"", {silent:true}).output;
+    if (line === "right") rl.close();
+        rl.prompt();
+}).on('close',function(){
+    process.exit(0);
+});
